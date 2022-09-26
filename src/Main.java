4e8f3af6cde6040e0ce18ddcef37fce7cfe8a7db
Sozonchik <den.sozon@mail.ru>
2022-09-26 23:14:32 +0500

    сделал 1.2

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args) {
         // Домашнее задание 1
         System.out.println("Задание 1.1");
-        int age = 15;
+        int age = 23;
         if (age >= 18) {
             System.out.println("Поздравляем с совершеннолетием");
         }
@@ -11,7 +11,21 @@ public class Main {
         }
 
         System.out.println("Задание 1.2");
-        if ()
+        if (age >= 7 && age < 18) {
+            System.out.println("Ребёнок ходит в школу");
+        }
+        if (age > 18 && age < 24) {
+            System.out.println("Человек может отправляться в университет");
+        }
+        if (age >=24) {
+            System.out.println("Человек окончил университет и ему пора искать первую работу");
+        }
+        if (age < 7) {
+            System.out.println("Очень мал");
+        }
+
+        System.out.println("Задание 1.3");
+
 
 
     }
