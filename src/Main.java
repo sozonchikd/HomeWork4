4d758c439eb83734b91542bc9e0ad8e46d63ef15
Sozonchik <den.sozon@mail.ru>
2022-09-26 23:03:32 +0500

    сделал 1.1

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,17 @@ public class Main {
     public static void main(String[] args) {
         // Домашнее задание 1
         System.out.println("Задание 1.1");
+        int age = 15;
+        if (age >= 18) {
+            System.out.println("Поздравляем с совершеннолетием");
+        }
+        if (age < 18) {
+            System.out.println("Возраст совершеннолетия еще не наступил");
+        }
+
+        System.out.println("Задание 1.2");
+        if ()
+
 
     }
 }
